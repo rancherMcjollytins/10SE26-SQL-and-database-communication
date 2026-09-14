25c385a91304809e63d9f166b6739b8778236ae5
ADRIAn <adrian.uy@education.nsw.gov.au>
2026-09-14 10:50:39 +1000

    Files made
    made some files and folders to store the activities, ran them and also changed name of 'school.db' to 'library.db'.
    Outputs the correct output, showing a connection is made and creates a school.db file if original is renamed or deleted.

--- activities-and-stuff/lesson-1/lesson1_connect.py
+++ activities-and-stuff/lesson-1/lesson1_connect.py
@@ -2,6 +2,8 @@
 
 import sqlite3
 
+print("The script creates a connection to a database called school.db, stored in a connection-variable called 'connection'. \nIf the database 'school.db' can't  be found (e.g. if we rename it to library), it will create the school.db file.")
+
 # Open a connection to school.db (SQLite creates the file if needed).
 connection = sqlite3.connect("school.db")
 print("Database connected!")

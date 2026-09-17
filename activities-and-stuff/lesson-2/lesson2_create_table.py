2701aff97e487632d6883cd517b70a57fede8d0c
adrian <adrian.uy@education.nsw.gov.au>
2026-09-17 15:10:13 +1000

    finished the python part of lesson 2
    :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface: :trollface:

---
+++ activities-and-stuff/lesson-2/lesson2_create_table.py
@@ -0,0 +1,29 @@
+
+import sqlite3
+
+# Connect Python to the SQLite database file.
+connection = sqlite3.connect("school.db")
+# A cursor is the object that sends SQL commands to the database.
+cursor = connection.cursor()
+
+# Execute SQL to create the students table if it does not exist yet.
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    year_group INTEGER,
+    criminal_record TEXT
+)
+""")
+
+# Clear old rows so this lesson script gives predictable output each run.
+cursor.execute("DELETE FROM students")
+
+# Insert rows using placeholders (?) to safely pass Python values.
+cursor.execute("INSERT INTO students (name, year_group) VALUES (?, ?)", ("Ava", 10))
+cursor.execute("INSERT INTO students (name, year_group, criminal_record) VALUES (?, ?, ?)", ("Leo", 63, "Wanted for 3 counts of Assault and Battery on police and 2 counts of Domestic Violence"))
+
+# Commit saves all changes made by INSERT/DELETE/CREATE statements.
+connection.commit()
+# Always close the connection when finished.
+connection.close()
---
+++ log.md
@@ -0,0 +1,5 @@
+## 17/09/2026
+### Where I am at
+finished most of lesson_01, just missing some of the documentation and question.
+### Session Goal 17/09/2026
+Goal: To complete lesson 02 (ext. and lesson 03) and have a sound understanding and execution of SQL and database manipulation in practice.

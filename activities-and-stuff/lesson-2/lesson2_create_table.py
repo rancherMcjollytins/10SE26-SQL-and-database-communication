77eb6a7bc3938d4d69289aca2ee9091796985a43
adrian <adrian.uy@education.nsw.gov.au>
2026-09-23 10:07:24 +1000

    Lesson 4 Completed, Notes Left to do
    Also added a range sort thing (from google and Gemini). :trollface:

--- activities-and-stuff/lesson-2/lesson2_create_table.py
+++ activities-and-stuff/lesson-2/lesson2_create_table.py
@@ -21,8 +21,9 @@ CREATE TABLE IF NOT EXISTS students (
 cursor.execute("DELETE FROM students")
 
 # Insert rows using placeholders (?) to safely pass Python values.
-cursor.execute("INSERT INTO students (name, year_group, age) VALUES (?, ?, ?)", ("Ava", 11, 16))
+cursor.execute("INSERT INTO students (name, year_group, age) VALUES (?, ?, ?)", ("Ava", 10, 16))
 cursor.execute("INSERT INTO students (name, year_group, age, criminal_record) VALUES (?, ?, ?, ?)", ("Leroy", 34, 45, "Wanted for Terrorism"))
+cursor.execute("INSERT INTO students (name, year_group, age, criminal_record) VALUES (?, ?, ?, ?)", ("Elliot Alderson", 32, 28, "Computer Fraud and Abuse, Money Laundering, Wire Fraud, Identity Theft, Computer Intrusion, Conspiracy to Murder, Phishing, Illegal Access to Private Data Servers, Trespassing, Unauthorised access, modification, or impairment of computer data/communications, Dishonestly obtaining or dealing in personal financial information, Extortion and Blackmail"))
 
 # Commit saves all changes made by INSERT/DELETE/CREATE statements.
 connection.commit()
--- activities-and-stuff/lesson-4/lesson4-filter.py
+++ activities-and-stuff/lesson-4/lesson4-filter.py
@@ -4,4 +4,34 @@ connectionVar =  _sqlite3.connect("school.db")
 
 SQLCursor = connectionVar.cursor()
 
-year_group_filter = 10
+year_group = 10
+SQLCursor.execute(
+    "SELECT year_group FROM STUDENTS WHERE year_group = ? ORDER BY year_group",
+    (year_group,)
+)
+
+
+rows = SQLCursor.fetchall()
+for row in rows:
+    print(row)
+
+SQLCursor.execute("SELECT COUNT(*) FROM students")
+total_students = SQLCursor.fetchone()[0]
+print("Total Students:", total_students)
+
+"""
+# for ranges according to gemini so be careful with ai code!!!!!!
+
+min_year = 6
+max_year = 32
+SQLCursor.execute(
+    "SELECT name, year_group FROM STUDENTS WHERE year_group BETWEEN ? AND ? ORDER BY NAME",
+    (min_year, max_year)
+)
+
+rows2 = SQLCursor.fetchall()
+for row2 in rows2:
+    print(row2)
+
+"""
+connectionVar.close()
